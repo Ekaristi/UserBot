9b7e80a0342245a939203a41b57ff10753a818f5
BL4CK_ID <58440372+BL4CKID@users.noreply.github.com>
2020-01-08 16:04:30 +0700

    Update weather.py

--- userbot/modules/weather.py
+++ userbot/modules/weather.py
@@ -124,15 +124,15 @@ async def get_weather(weather):
     await weather.edit(
         f"**{desc}**\n" +
         f"{cityname}, {fullc_n}\n" + f"{time}\n\n" +
-        f"**Temperature:** {celsius(curtemp)}°C | {fahrenheit(curtemp)}°F\n"
+        f"**Temperature :** {celsius(curtemp)}°C | {fahrenheit(curtemp)}°F\n"
         +
-        f"**Min. Temp.:** {celsius(min_temp)}°C | {fahrenheit(min_temp)}°F\n"
+        f"**Min. Temp. :** {celsius(min_temp)}°C | {fahrenheit(min_temp)}°F\n"
         +
-        f"**Max. Temp.:** {celsius(max_temp)}°C | {fahrenheit(max_temp)}°F\n"
-        + f"**Humidity:** {humidity}%\n" +
-        f"**Wind:** {kmph[0]} kmh | {mph[0]} mph, {findir}\n" +
-        f"**Sunrise:** {sun(sunrise)}\n" +
-        f"**Sunset:** {sun(sunset)}")
+        f"**Max. Temp. :** {celsius(max_temp)}°C | {fahrenheit(max_temp)}°F\n"
+        + f"**Humidity :** {humidity}%\n" +
+        f"**Wind :** {kmph[0]} kmh | {mph[0]} mph, {findir}\n" +
+        f"**Sunrise :** {sun(sunrise)}\n" +
+        f"**Sunset :** {sun(sunset)}")
 
 
 """CMD_HELP.update({
